15027b3ab6c31d97fec97a572a07671ecb56c37f
Mousa Hammouz <mosahammouz2004@gmail.com>
2026-07-20 14:53:41 +0300

    applied idempotent concept in creation tabels and database

--- DataBase/CreateDataBase.sql
+++ DataBase/CreateDataBase.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE Reservations;
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.databases
+    WHERE name = 'Reservations'
+)
+    BEGIN
+        CREATE DATABASE Reservations;
+    END;
 GO
--- DataBase/CreateTables.sql
+++ DataBase/CreateTables.sql
@@ -1,6 +1,12 @@
 USE Reservations;
 GO
 
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Restaurants'
+)
+BEGIN
 CREATE TABLE Restaurants (
 RestaurantId INT PRIMARY KEY IDENTITY(1,1),
 Name NVARCHAR(100) NOT NULL,
@@ -8,8 +14,16 @@ Address NVARCHAR(255),
 PhoneNumber NVARCHAR(20),
 OpeningHours NVARCHAR(100)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Customers'
+)
+BEGIN
 CREATE TABLE Customers (
 CustomerId INT PRIMARY KEY IDENTITY(1,1),
 FirstName NVARCHAR(50) NOT NULL,
@@ -17,11 +31,19 @@ LastName NVARCHAR(50) NOT NULL,
 Email NVARCHAR(100),
 PhoneNumber NVARCHAR(20)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Employees'
+)
+BEGIN
 CREATE TABLE Employees (
 EmployeeId INT PRIMARY KEY IDENTITY(1,1),
-RestaurantId INT NOT NULL, --FK
+RestaurantId INT NOT NULL,
 FirstName NVARCHAR(50) NOT NULL,
 LastName NVARCHAR(50) NOT NULL,
 Position NVARCHAR(50),
@@ -30,24 +52,40 @@ CONSTRAINT FK_Employees_Restaurants
 FOREIGN KEY (RestaurantId)
 REFERENCES Restaurants(RestaurantId)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Tables'
+)
+BEGIN
 CREATE TABLE Tables (
 TableId INT PRIMARY KEY IDENTITY(1,1),
-RestaurantId INT NOT NULL, --FK
+RestaurantId INT NOT NULL,
 Capacity INT NOT NULL,
 
 CONSTRAINT FK_Tables_Restaurants
 FOREIGN KEY (RestaurantId)
 REFERENCES Restaurants(RestaurantId)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Reservations'
+)
+BEGIN
 CREATE TABLE Reservations (
 ReservationId INT PRIMARY KEY IDENTITY(1,1),
-CustomerId INT NOT NULL,  --FK
-RestaurantId INT NOT NULL, --FK
-TableId INT NOT NULL,  --FK
+CustomerId INT NOT NULL,
+RestaurantId INT NOT NULL,
+TableId INT NOT NULL,
 ReservationDate DATETIME NOT NULL,
 PartySize INT NOT NULL,
 
@@ -63,8 +101,16 @@ CONSTRAINT FK_Reservations_Tables
 FOREIGN KEY (TableId)
 REFERENCES Tables(TableId)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'MenuItems'
+)
+BEGIN
 CREATE TABLE MenuItems (
 ItemId INT PRIMARY KEY IDENTITY(1,1),
 RestaurantId INT NOT NULL,
@@ -76,8 +122,16 @@ CONSTRAINT FK_MenuItems_Restaurants
 FOREIGN KEY (RestaurantId)
 REFERENCES Restaurants(RestaurantId)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'Orders'
+)
+BEGIN
 CREATE TABLE Orders (
 OrderId INT PRIMARY KEY IDENTITY(1,1),
 ReservationId INT NOT NULL,
@@ -93,8 +147,16 @@ CONSTRAINT FK_Orders_Employees
 FOREIGN KEY (EmployeeId)
 REFERENCES Employees(EmployeeId)
 );
+END;
 GO
 
+
+IF NOT EXISTS (
+SELECT 1
+FROM sys.tables
+WHERE name = 'OrderItems'
+)
+BEGIN
 CREATE TABLE OrderItems (
 OrderItemId INT PRIMARY KEY IDENTITY(1,1),
 OrderId INT NOT NULL,
@@ -109,4 +171,5 @@ CONSTRAINT FK_OrderItems_MenuItems
 FOREIGN KEY (ItemId)
 REFERENCES MenuItems(ItemId)
 );
+END;
 GO
