50254fd254ae54c683499242bb15e227c1811130
Mousa Hammouz <mosahammouz2004@gmail.com>
2026-07-21 12:43:16 +0300

    list of menu items by reservations id. QUERY 04

---
+++ Queries/04_ListOfMenuItemsByReservationId.sql
@@ -0,0 +1,16 @@
+DECLARE @reservation_id INT =5;
+
+SELECT Reservations.ReservationId,
+       MenuItems.*
+FROM Reservations
+
+JOIN Orders
+ON Orders.ReservationId = Reservations.ReservationId
+
+JOIN OrderItems
+ON OrderItems.OrderId = Orders.OrderId
+
+JOIN MenuItems
+ON MenuItems.ItemId = OrderItems.ItemId
+
+WHERE Reservations.ReservationId = @reservation_id;
